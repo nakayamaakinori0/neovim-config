bd579ec182ab650094334b15af5967c5f3a39a26
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2026-01-31 18:07:28 +0900

    update vimconfig

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,26 +14,11 @@ map("n", "<C-j>", "<C-w>j", { desc = "Go to lower window" })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to upper window" })
 map("n", "<C-l>", "<C-w>l", { desc = "Go to right window" })
 
--- Buffers
-map("n", "<leader>bl", ":Neotree buffers<CR>", { desc = "List Buffers" })
-map("n", "<leader>bD", ":% bd<CR>")
-
--- file explorer
--- map("n", "<leader>e", ":Neotree toggle<CR>", { desc = "Explorer Toggle" })
--- map("n", "<leader>E", ":Neotree reveal<CR>", { desc = "Explorer Toggle current file" })
 map("n", "<leader>e", ":Oil<CR>", { desc = "Open parent directory" })
 
 -- Range indent
 map("v", "<", "<gv")
 map("v", ">", ">gv")
-
--- Location list
-map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location List" })
-map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix List" })
-
--- Diagnostic keymaps
--- map("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
--- map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 
 -- Windows
 map("n", "<leader>s", ":split<CR>", { desc = "Split window below", remap = true })
@@ -48,6 +33,7 @@ vim.keymap.set("n", "L", "$")
 vim.keymap.set("v", "H", "0")
 vim.keymap.set("v", "L", "$")
 
+vim.keymap.set("n", "<leader>tt", ":term<CR>", { desc = "Open terminal" })
 vim.keymap.set("n", "<leader>tk", ":Telekasten panel<CR>")
 
 vim.keymap.set("n", "<leader>td", function()
