5a8fd7de33df4e8979630e003ac8212d711536a4
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2026-02-03 01:50:44 +0900

    update formatter eslint_dがいいかんじ

--- lua/plugins/code/conform.lua
+++ lua/plugins/code/conform.lua
@@ -1,7 +1,6 @@
 return {
   "stevearc/conform.nvim",
-  lazy = true,
-  cmd = { "ConformInfo" },
+  lazy=false,
   keys = {
     {
       "<leader>cf",
@@ -12,18 +11,21 @@ return {
     },
   },
   opts = {
-    format_on_save = false,
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
     -- フォーマッターを追加する場合、masonでインストール後ここに追記する
     formatters_by_ft = {
       lua = { "stylua" },
       sh = { "shfmt" },
       fish = { "fish_indent" },
-      javascript = { "prettier" },
-      javascriptreact = { "prettier" },
-      typescript = { "prettier" },
-      typescriptreact = { "prettier" },
-      vue = { "prettier" },
-      markdown = { "prettier" },
+      javascript = { "eslint_d" },
+      javascriptreact = { "eslint_d" },
+      typescript = { "eslint_d" },
+      typescriptreact = { "eslint_d" },
+      vue = { "eslint_d" },
+      -- markdown = { "prettier" },
     },
   },
 }
