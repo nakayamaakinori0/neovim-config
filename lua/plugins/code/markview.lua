de4e101cc38676297e5db051988751031e878c00
Ak norinori <42559915+nakayamaakinori0@users.noreply.github.com>
2026-02-28 03:10:12 +0900

    markviewのトグルコマンド

--- lua/plugins/code/markview.lua
+++ lua/plugins/code/markview.lua
@@ -2,6 +2,9 @@
 return {
   "OXY2DEV/markview.nvim",
   lazy = false,
+  keys = {
+    { "<localleader>mv", "<cmd>Markview Toggle<cr>", desc = "MarkView Toggle" },
+  },
 
   -- Completion for `blink.cmp`
   -- dependencies = { "saghen/blink.cmp" },
