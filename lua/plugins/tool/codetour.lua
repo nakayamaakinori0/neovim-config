aa00e95fe21a508eaa0461ddabca16896f870153
nakayama-akinori <nakayama_a@kokopelli-inc.com>
2026-03-03 13:39:32 +0900

    codetourのkeybind

--- lua/plugins/tool/codetour.lua
+++ lua/plugins/tool/codetour.lua
@@ -4,8 +4,8 @@ return {
   lazy = true,
   cmd = { "TourNext", "TourPrev", "TourLoad" },
   keys = {
-    { "<leader>tn", "<cmd>TourNext<cr>", desc = "Next tour step" },
-    { "<leader>tp", "<cmd>TourPrev<cr>", desc = "Previous tour step" },
+    { "<localleader>tn", "<cmd>TourNext<cr>", desc = "Next tour step" },
+    { "<localleader>tp", "<cmd>TourPrev<cr>", desc = "Previous tour step" },
   },
   config = function()
     require("custom.codetour").setup()
