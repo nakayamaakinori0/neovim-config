83309eddf01f2b49da0035227e55d99495613dbb
nakayama-akinori <nakayama_a@kokopelli-inc.com>
2026-03-03 12:09:41 +0900

    tokyonightの背景透過

--- lua/plugins/ui/tokyonight.lua
+++ lua/plugins/ui/tokyonight.lua
@@ -2,7 +2,15 @@ return {
   "folke/tokyonight.nvim",
   lazy = false, -- 起動時に即座に読み込み
   priority = 1000, -- 他のプラグインより先に読み込み
-  config = function()
+  opts = {
+    transparent = true, -- 背景を透明にする
+    styles = {
+      sidebars = "transparent", -- サイドバーを透明にする
+      floats = "transparent", -- 浮動ウィンドウを透明にする
+    },
+  },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
     vim.cmd([[colorscheme tokyonight-moon]])
   end,
 }
